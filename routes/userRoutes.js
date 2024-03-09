c5039cc98c73ddb47b030365d7e4fc99b0dfcfc7
abdul-SE-Flutter <honeyhannan65@gmail.com>
2024-03-09 06:40:38 -0800

    refactored code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,4 +3,5 @@ const userController = require("../controllers/user/user");
 const router = express.Router();
 router.get("/programs", userController.getPrograms);
 router.get("/program/:programId", userController.getSingleProgram);
+router.post("/hire/expert/:userId",userController.hireExpert);
 module.exports = router;
